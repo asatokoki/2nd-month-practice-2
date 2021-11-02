efa9ebab400595ea3b980fcaf6bd095e493ecb56
asatokoki <okometabero@me.com>
2021-11-02 05:19:15 +0000

    [Fix]ユーザー情報変更後のURLを正しくした（あとエラー８こ）

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root 'homes#top'
+  get 'home/about' => 'homes#about'
+
   devise_for :users
 
   resources :users,only: [:show,:index,:edit,:update]
   resources :books
-  get 'home/about' => 'homes#about'
 
 end
